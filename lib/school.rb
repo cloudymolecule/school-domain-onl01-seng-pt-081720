e8f90b265ecc10b1fd4142710e8fd1ed5be3a2c3
Dario Carlino <baires82@gmail.com>
2020-08-31 22:32:57 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -5,7 +5,11 @@ class School
     @roster = {}
   end
   def add_student(name, grade)
-    @roster[grade] = []
-    @roster[grade] = [name]
+    if @roster[grade]
+      #nothing
+    else
+      @roster[grade] = []
+      @roster[grade] = [name]
+    end
   end
 end
